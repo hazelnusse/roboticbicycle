4e0e274f27a3001cd93f281f7b98c43076159740
Dale Lukas Peterson <hazelnusse@gmail.com>
2011-07-11 15:55:12 -0700

    I2C@400khz works with magnetometer.

--- MCP/src/stickimu_test.cpp
+++ MCP/src/stickimu_test.cpp
@@ -5,118 +5,15 @@
 #include "stm32f10x_i2c.h"
 #include "hmc5843.h"
 
-void GPIO_Configuration(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  /* Configure PB6, PB7 to alternate function open drain mode, 50MHz */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-}
-
-void HMC_Config_RA(void)
-{
-  // START == Master pulls SDA Low then pulls SCL LOW
-  I2C_GenerateSTART(I2C1, ENABLE);
-
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
-  I2C_Send7bitAddress(I2C1, HMC_ADDR_W, I2C_Direction_Transmitter);
-  // Getting stuck here -- probably due to Slave missing the address and not
-  // sending the ACK.  Need to verify with logic analyser and see if sending
-  // a stop condition will fix this.
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {}
-  I2C_SendData(I2C1, HMC_CRA);  // Write to CRA
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-  I2C_SendData(I2C1, FIFTY_HZ | NORMAL_MODE);  // Set to 50Hz, normal mode
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-
-  // STOP == Master releases SCL, then releases SDA, pull up-resistors relied
-  // upon to pull these lines high.
-  I2C_GenerateSTOP(I2C1, ENABLE);
-}
-
-void HMC_Config_RB(void)
-{
-  // START == Master pulls SDA Low then pulls SCL LOW
-  I2C_GenerateSTART(I2C1, ENABLE);
-
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
-  I2C_Send7bitAddress(I2C1, HMC_ADDR_W, I2C_Direction_Transmitter);
-  // Getting stuck here -- probably due to Slave missing the address and not
-  // sending the ACK.  Need to verify with logic analyser and see if sending
-  // a stop condition will fix this.
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {}
-  I2C_SendData(I2C1, HMC_CRB);  // Write to CRB
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-  I2C_SendData(I2C1, GAIN_1620);  // Highest gain mode
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-
-  // STOP == Master releases SCL, then releases SDA, pull up-resistors relied
-  // upon to pull these lines high.
-  I2C_GenerateSTOP(I2C1, ENABLE);
-}
-
-void HMC_Config_MR(void)
-{
-  // START == Master pulls SDA Low then pulls SCL LOW
-  I2C_GenerateSTART(I2C1, ENABLE);
-  // Second START condition should reset I2C bus, shouldn't be needed
-  //I2C_GenerateSTART(I2C1, ENABLE);
-
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
-  I2C_Send7bitAddress(I2C1, HMC_ADDR_W, I2C_Direction_Transmitter);
-  // Getting stuck here -- probably due to Slave missing the address and not
-  // sending the ACK.  Need to verify with logic analyser and see if sending
-  // a stop condition will fix this.
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {}
-  I2C_SendData(I2C1, HMC_MR);  // Write to MODE register
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-  I2C_SendData(I2C1, MODE_CONTINUOUS);  // Set to continuous conversion mode
-  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED)) {}
-
-  // STOP == Master releases SCL, then releases SDA, pull up-resistors relied
-  // upon to pull these lines high.
-  I2C_GenerateSTOP(I2C1, ENABLE);
-}
-
-void RCC_Configuration(void)
-{
-  /* I2C1 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
-
-  /* GPIOB clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-}
-
-void I2C_Configuration(void)
-{
-  I2C_InitTypeDef I2C_InitStructure;
-
-  // Disable I2C1 before changing any of the settings
-  I2C_DeInit(I2C1);
-  I2C_Cmd(I2C1, DISABLE);
-
-  // Populate 6 fields of I2C_InitStructure
-  I2C_InitStructure.I2C_ClockSpeed = 400000;  // 100kHz
-  I2C_InitStructure.I2C_Mode = I2C_Mode_I2C; // I2C mode
-  I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_16_9;  // Doesn't matter
-  I2C_InitStructure.I2C_OwnAddress1 = 0x00;           // Doesn't matter
-  I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;         // Enable ACK on 9th clock cycle
-  I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-  // Write settings to I2C Registers
-  I2C_Init(I2C1, &I2C_InitStructure);
-
-  // I2C Error interrupt mask
-  // I2C_ITConfig(I2C1, I2C_IT_BUF|I2C_IT_EVT|I2C_IT_ERR, ENABLE);
-
-  // Enable I2C1
-  //I2C_SoftwareResetCmd(I2C1, ENABLE);
-  //I2C_SoftwareResetCmd(I2C1, DISABLE);
-  I2C_Cmd(I2C1, ENABLE);
-}
+// extern void I2C1_EV_IRQHandler(void);
+//void I2C1_ER_IRQHandler(void);
+void GPIO_Configuration(void);
+void HMC_Configuration(void);
+void HMC_Verify_Configuration(uint8_t * data);
+void HMC_ReadXYZ(uint8_t * data);
+void RCC_Configuration(void);
+void I2C_Configuration(void);
+void delay(int N);
 
 int main(void)
 {
@@ -125,20 +22,187 @@ int main(void)
   I2C_Configuration();
 
   // Setup HMC
-  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
-  HMC_Config_RA();
-  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
-  HMC_Config_RB();
-  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
-  HMC_Config_MR();
+  HMC_Configuration();
 
   // Read data
-  uint8_t data[6] = {0, 0, 0, 0, 0, 0};
+  //int16_t data[3] = {0, 0, 0};
+  uint8_t data[9] = {2};
   while (1) {
-    // wait on I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED
-    // wait for the byte I2C_EVENT_MASTER_BYTE_RECEIVED
-    // receive the data:  uint8_t I2C_ReceiveData()
+    HMC_Verify_Configuration(data);
+    // HMC_ReadXYZ((uint8_t *) data);
+    delay(1000);
   }
 
   return 0;
 }
+
+void delay(int N)
+{
+  for (int i = 0; i < N; ++i)
+    asm("nop");
+}
+
+void I2C_Configuration(void)
+{
+  // Uncomment to turn on interrupts
+  // NVIC_EnableIRQ(I2C1_EV_IRQn);
+  // NVIC_EnableIRQ(I2C1_ER_IRQn);
+  I2C_InitTypeDef I2C_InitStructure;
+
+  // Disable I2C1 before changing any of the settings
+  I2C_DeInit(I2C1);
+  I2C_Cmd(I2C1, DISABLE);
+
+  // Populate 6 fields of I2C_InitStructure
+  I2C_InitStructure.I2C_ClockSpeed = 400000;  // 400kHz
+  I2C_InitStructure.I2C_Mode = I2C_Mode_I2C; // I2C mode
+  I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_16_9;  // Doesn't matter unless we are in fast mode
+  I2C_InitStructure.I2C_OwnAddress1 = 0x00;           // Doesn't matter
+  I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;         // Enable ACK on 9th clock cycle
+  I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+  // Write settings to I2C Registers
+  I2C_Init(I2C1, &I2C_InitStructure);
+
+  // I2C Error interrupt mask
+  I2C_ITConfig(I2C1, I2C_IT_BUF|I2C_IT_EVT|I2C_IT_ERR, ENABLE);
+
+  // Enable I2C1
+  I2C_Cmd(I2C1, ENABLE);
+}
+
+void RCC_Configuration(void)
+{
+  // I2C1 clock enable
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+
+  // GPIOB clock enable
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+}
+
+void GPIO_Configuration(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  // Configure PB6, PB7 to alternate function open drain mode, 50MHz
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+  // Set SCL and SDA, this should release both lines to be high
+  GPIOB->BSRR = 0x00C0;
+}
+
+// Writes to CRA, CRB, and MR to put the HMC5843L in the following mode
+// -- 50 Hz Output Data Rate
+// -- Normal measurement
+// -- Gain 390 / +/- 4.5Ga sensitivity
+// -- Continuous - Conversion mode
+void HMC_Configuration(void)
+{
+  // Wait for the I2C lines to be free
+  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
+
+  // Generate a I2C Start condition (Master pulls SDA LOW while SCL is HIGH)
+  I2C_GenerateSTART(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
+
+  // Send the HMC Write address
+  I2C1->DR = HMC_ADDR_W;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {}
+
+  // Send the address of Configuration Register A
+  I2C1->DR = HMC_CRA;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING)) {}
+
+  // 50 Hz, Normal Mode
+  I2C1->DR = FIFTY_HZ | NORMAL_MODE;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING)) {}
+
+  // Set Gain
+  I2C1->DR = GAIN_390;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING)) {}
+
+  // Set Gain
+  I2C1->DR = MODE_CONTINUOUS; // MODE_SINGLE once we get FreeIMU
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING)) {}
+
+  // Generate a I2C Stop condition (Master releases SDA while SCL is HIGH)
+  I2C_GenerateSTOP(I2C1, ENABLE);
+}
+
+void HMC_Verify_Configuration(uint8_t * data)
+{
+  I2C_AcknowledgeConfig(I2C1, ENABLE);
+  // Wait for the I2C lines to be free
+  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
+
+  // Generate a I2C Start condition (Master pulls SDA LOW while SCL is HIGH)
+  I2C_GenerateSTART(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
+  
+  // Send the HMC Write address
+  I2C1->DR = HMC_ADDR_W;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) {}
+
+  // Send the address of Configuration Register A
+  I2C1->DR = HMC_CRA;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING)) {}
+
+  // Send a repeated start
+  I2C_GenerateSTART(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
+
+  // Send the HMC Read address
+  I2C1->DR = HMC_ADDR_R;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {}
+
+  // Need to check that master sends an ACK (low byte)
+  for (int i = 0; i < 8; ++i) {
+    while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED)) {}
+    data[i] = (uint8_t) I2C1->DR;
+  }
+  I2C_AcknowledgeConfig(I2C1, DISABLE);
+  I2C_GenerateSTOP(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED)) {}
+  data[8] = (uint8_t) I2C1->DR;
+}
+
+
+
+void HMC_ReadXYZ(uint8_t * data)
+{
+  // Wait for the I2C lines to be free
+  while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY)) {}
+
+  // Generate a I2C Start condition (Master pulls SDA LOW while SCL is HIGH)
+  I2C_GenerateSTART(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT)) {}
+
+  // Send the HMC Read address
+  I2C1->DR = HMC_ADDR_R;
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) {}
+
+  // Need to check that master sends an ACK (low byte)
+  for (int i = 0; i < 5; ++i) {
+    while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED)) {}
+    data[i] = (uint8_t) I2C1->DR;
+  }
+  I2C_AcknowledgeConfig(I2C1, DISABLE);
+  I2C_GenerateSTOP(I2C1, ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED)) {}
+  data[5] = (uint8_t) I2C1->DR;
+}
+
+extern "C" {
+void I2C1_EV_IRQHandler(void)
+{
+  // volatile uint32_t flag = I2C_GetLastEvent(I2C1);
+  while (1) {}
+}
+}
+//void I2C1_ER_IRQHandler(void)
+//{
+//  volatile uint32_t flag = I2C_GetLastEvent(I2C1);
+//  while (1) {}
+//}
